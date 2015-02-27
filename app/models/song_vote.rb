a9cc3a9d136aa0f6cc73e3dfba49f8368ca9b109
Ian Gawronski <iangawronski@gmail.com>
2015-02-27 13:42:03 -0500

    starting on votes

--- app/models/song_vote.rb
+++ app/models/song_vote.rb
@@ -1,4 +1,13 @@
 class SongVote < ActiveRecord::Base
   belongs_to :user
   belongs_to :song
+  belongs_to :playlist
 end
+
+
+def self.vote(song, user)
+    vote = SongVote.new()
+    vote.user_id = user.id
+    vote.song_id = song.id
+    vote.save
+  end
